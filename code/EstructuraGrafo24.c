c5201eda66481c756a1c8669686e3e77175d629f
juampiludu <juanzakka@gmail.com>
2024-03-30 11:16:47 -0300

    correccion de errores

--- code/EstructuraGrafo24.c
+++ code/EstructuraGrafo24.c
@@ -36,17 +36,17 @@ Grafo ConstruirGrafo() {
     u32 cantidadVertices = 0, cantidadLados = 0;
     u32 vert1, vert2;
     u32 indiceLado = 0, ladosLeidos = 0;
-    char line[MAX_LINE_SIZE], edge[4];
+    char line[MAX_LINE_SIZE], edge[5];
     char p;
     lado *listaLados = NULL;
-    while (fgets(line, MAX_LINE_SIZE, stdin) && ladosLeidos <= cantidadLados) {
+    while (fgets(line, MAX_LINE_SIZE, stdin)) {
         // salteamos todos los comentarios
         if (line[0] == 'c' || line[0] == '\n') {
             continue;
 
         // procesamos la cantidad de vertices y lados
         } else if (line[0] == 'p'){
-            if (sscanf(line, "%c %s %u %u", &p, edge, &cantidadVertices, &cantidadLados) != 4)
+            if (sscanf(line, "%c %4s %u %u", &p, edge, &cantidadVertices, &cantidadLados) != 4)
                 return NULL;
             if (cantidadVertices < 2 || cantidadLados == 0) 
                 return NULL;
@@ -68,7 +68,9 @@ Grafo ConstruirGrafo() {
             listaLados[++indiceLado] = lado2; // lado2 = (v->vert2, w->vert1)
             indiceLado++;
             ladosLeidos++;
-        
+
+            if (ladosLeidos == cantidadLados)
+                break;   
         } else {
             return NULL;
         }
@@ -177,8 +179,7 @@ u32 Grado(u32 i, Grafo G) {
     if (G==NULL) 
         exit(EXIT_FAILURE);
         
-    u32 numeroVertices = NumeroDeVertices(G);
-    if (i >= numeroVertices)
+    if (i >= NumeroDeVertices(G))
         return NULL_COLOR;
     return G->vertices[i]->grado;
 }
@@ -187,8 +188,8 @@ u32 Vecino(u32 j, u32 i, Grafo G) {
     if (G==NULL) 
         exit(EXIT_FAILURE);
 
-    if (i > NumeroDeVertices(G) && j > NumeroDeVertices(G))
-        exit(EXIT_FAILURE);
+    if (i >= NumeroDeVertices(G) && j >= NumeroDeVertices(G))
+        return -1;
 
     if (i >= NumeroDeVertices(G) || (i < NumeroDeVertices(G) && j >= Grado(i, G)))
         return -1;
--- code/main.c
+++ code/main.c
@@ -3,13 +3,34 @@
 #include "APIG24.h"
 #include "EstructuraGrafo24.h"
 
-int main() {
-    Grafo grafo = ConstruirGrafo();
-    u32 n = NumeroDeVertices(grafo);
-    u32 m = NumeroDeLados(grafo);
-    u32 delta = Delta(grafo);
+void InfoGrafo(Grafo G) {
+    printf("\n///// Informacion del Grafo /////\n\n");
+    printf(" - Numero de vertices: %u\n", NumeroDeVertices(G));
+    printf(" - Numero de lados:    %u\n", NumeroDeLados(G));
+    printf(" - Delta:              %u\n", Delta(G));
+    printf("\n/////////////////////////////////\n");
+}
 
-    printf("n = %u, m = %u, delta = %u\n", n, m, delta);
+// void PrintEstructuraGrafo(Grafo G) {
+//     printf("Vertice | Color  | Vecinos\n");
+//     printf("---------------------------\n");
+//     for (u32 i = 0; i < NumeroDeVertices(G); i++) {
+//         printf("   %c   | %-6s | ", //vertice, //color);
+//         for (int j = 0; j < //grado; j++) {
+//             printf("%c", //vecinos);
+//             if (j < //grado - 1)
+//                 printf(", ");
+//         }
+//         printf("\n");
+//     }
+// }
+
+int main() {
+    printf("Construyendo grafo...\n\n");
+    Grafo grafo = ConstruirGrafo();
+    // u32 n = NumeroDeVertices(grafo);
+
+    InfoGrafo(grafo);
 
     DestruirGrafo(grafo);
 
